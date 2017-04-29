47022491b02b3c094deefd4ad2f37527f8f285cf
Duncan Smith <hi@dms.sh>
2017-04-29 12:24:29 -0700

    Only enable debug panel in DEBUG mode

--- FaceyFindy/AppDelegate.swift
+++ FaceyFindy/AppDelegate.swift
@@ -16,8 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    let shakeTrigger = DBShakeTrigger()
-    DBDebugToolkit.setup(with: [shakeTrigger])
+    
+    #if DEBUG
+      let shakeTrigger = DBShakeTrigger()
+      DBDebugToolkit.setup(with: [shakeTrigger])
+    #endif
     
     return true
   }
